008ea997de8d0f7ce0385ddb2da5dfc8b588ddb1
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-10-25 20:33:57 +0545

    Adding components in Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+       <header className='header'>
+        <section>
+            <div>
+                <h2>Little Lemon</h2>
+                <h2>Butwal</h2>
+                
+            </div>
+        </section>
+       </header>
+    );
+};
+
+export default Header;
