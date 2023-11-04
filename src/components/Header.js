b480ccf3cf4a61758a977ec0ee20de5c6cb9ee50
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-11-04 17:03:58 +0545

    Adding image in little lemon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import bannerImg from '../images/restauranfood.jpg';
 
 const Header = () => {
-    return (
-       <header className='header'>
-        <section>
-            <div>
-                <h2>Little Lemon</h2>
-                <h2>Butwal</h2>
-                
-            </div>
-        </section>
-       </header>
-    );
+  return (
+    <header className="header">
+      <section>
+        {/* Texts */}
+        <div className="banner">
+          <h2>Little Lemon</h2>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional recipes servred with a modern twist.
+          </p>
+          <Link to="/booking">
+            <button aria-label="On Click">Reserve Table</button>
+          </Link>
+        </div>
+
+        {/* Image */}
+        <div className="banner-img">
+          <img src={bannerImg} alt="" />
+        </div>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
