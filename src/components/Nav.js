99e499696596b12b59011785c4daff8831e518a4
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-11-04 20:43:24 +0545

    Adding components in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,58 +1,53 @@
-import React, {useState} from "react";
-import logo from "../images/Logo .svg";
-
+import React, { useState } from 'react';
+import logo from '../images/Logo .svg';
 
 const Nav = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    }
-    return (
-        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
 
-            <a href="/" className="logo">
-                <img src={logo} alt="logo"/>
-            </a>
+      {/* Mobile navbar */}
+      <div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
 
-            {/* Mobile navbar */}
-            <div>
-                <div className="menu-icon" onClick={toggleMenu}>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                </div>
-            </div>
-
-            {/* Nav items */}
-            <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
-                <li>
-                    < a href="/">Home</a>
-                </li>
-                <li>
-                    < a href="/">About</a>
-                </li>
-                <li>
-                    < a href="/">Services</a>
-                </li>
-                <li>
-                    < a href="/">Menu</a>
-                </li>
-                <li>
-                    < a href="/">Reservations</a>
-                </li>
-                <li>
-                    < a href="/">Order Online</a>
-                </li>
-                <li>
-                    < a href="/">Login</a>
-                </li>
-            </ul>
-
-
-        </nav>
-
-    );
+      {/* Nav items */}
+      <ul className={`nav-links ${menuOpen ? 'visible' : ''}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
