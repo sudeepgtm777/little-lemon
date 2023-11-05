813cd15ac2f398093cdb3619539f45c0c834f9f1
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-11-05 17:05:17 +0545

    Debug in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,14 @@ const Header = () => {
   return (
     <header className="header">
       <section>
-        {/* Texts */}
         <div className="banner">
           <h2>Little Lemon</h2>
-          <h2>Chicago</h2>
+          <h3>Chicago</h3>
           <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes servred with a modern twist.</p>
           <Link to="/booking">
             <button aria-label="On Click">Reserve Table</button>
           </Link>
         </div>
-
-        {/* Image */}
         <div className="banner-img">
           <img src={bannerImg} alt="" />
         </div>
