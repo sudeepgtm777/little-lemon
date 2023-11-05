7b7b9ee8880f5ae002bf50a3cb63a31a23ac0d78
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-11-05 15:06:59 +0545

    Debug in Booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BookingForm from './BookingForm';
 
-const Booking = props => {
+const Booking = (props) => {
   return <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} SubmitForm={props.SubmitForm} />;
 };
 
